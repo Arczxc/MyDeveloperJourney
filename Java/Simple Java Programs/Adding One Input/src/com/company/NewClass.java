edaa31054afa80b7db95af705a55dbd92e9a283b
John Nathaniel Arcilla <69438647+Arczxc@users.noreply.github.com>
2021-07-12 16:06:55 +0800

    add

--- Java/Simple Java Programs/Adding One Input/src/com/company/NewClass.java
+++ Java/Simple Java Programs/Adding One Input/src/com/company/NewClass.java
@@ -1,2 +1,14 @@
-package com.company;public class NewClass {
+package com.company;
+
+public class NewClass {
+    int sum = 0;
+    void addNum (String s)
+    {
+        for (int i = 0 ; i < s.length();i++)
+        {
+            sum += s.charAt(i) - 48;
+        }
+        System.out.println("The Sum is : " + sum);
+    }
+
 }
